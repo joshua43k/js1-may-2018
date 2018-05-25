eca5f2271ef7b3c90d84c2389dff3e3529c859f3
Ryeker <ryekerjh@gmail.com>
2018-05-24 19:46:19 -0700

    day 1 in class work

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var myName = "Ryeker"; //camel casing is the standard way of writing values in JS. Lowercase first letter, uppercase following leading letters.
+
+//Single Line comment. 
+
+/*
+Multi-Line
+comments
+are 
+written 
+like 
+this.
+*/
+console.log(myName); //All statements in JS must end wtih a semi-colon (;).
