8e3d9fb04fd57b4cbaf7092c91d2dd582343fb4e
Ryeker <ryekerjh@gmail.com>
2018-06-27 18:33:15 -0700

    adding day 11

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+function calculateVisits(returnUser) {
+  let visitCount = 0;
+  visitCount++;
+  function logCount() {
+    console.log(visitCount);
+  }
+  return logCount();
+  if(returnUser) return visitCount
+
+}
+
+calculateVisits(true);
+
+
+import React from 'react';
+
+export default App extends Component {
+  render();
+  return (
+    <Component />
+  )
+}
+
+function Component(one, two three) {
+  this.one = one;
+  // .....
+}
+new Component(render(), template)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
